00f1b93861192f07e48e9eabf57d492841e8f35d
Max Cpe <ratipong_max@hotmail.com>
2019-04-18 22:50:34 +0700

    create repository

---
+++ src/main/java/com/maxcpe/springsecurityoauth2login/SpringSecurityOauth2LoginApplication.java
@@ -0,0 +1,13 @@
+package com.maxcpe.springsecurityoauth2login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2LoginApplication.class, args);
+	}
+
+}
